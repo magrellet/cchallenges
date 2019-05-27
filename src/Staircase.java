316fd57dd7f50586ee276164b00264b2fee75ef3
Martin <martin.alejandro.grellet@gmail.com>
2019-05-27 00:10:25 -0300

    HRank - Staircase

---
+++ src/Staircase.java
@@ -0,0 +1,77 @@
+/*Consider a staircase of size: n=4
+
+        #
+        ##
+        ###
+        ####
+        Observe that its base and height are both equal to,and the image is drawn using # symbols and spaces.The last line is not preceded by any spaces.
+
+        Write a program that prints a staircase of size.
+
+        Function Description
+
+        Complete the staircase function in the editor below.It should print a staircase as described above.
+
+        staircase has the following parameter(s):
+
+        n:an integer
+        Input Format
+
+        A single integer,n,denoting the size of the staircase.
+
+        Constraints
+
+        .0<n=<100
+
+        Output Format
+
+        Print a staircase of size using # symbols and spaces.
+
+        Note:The last line must have spaces in it.
+
+        Sample Input
+
+        6
+        Sample Output
+
+        #
+        ##
+        ###
+        ####
+        #####
+        ######
+        Explanation
+
+        The staircase is right-aligned,composed of # symbols and spaces,and has a height and width of. n=6
+
+ */
+
+public class Staircase {
+    public static void main(String[] args) {
+        staircase(6);
+    }
+
+    static void staircase(int n) {
+        String space = "";
+        String symbol ="#";
+
+        for(int i =n;i>=1;i--){
+
+            while(true){
+                if(space.length()<i){
+                    space+=" ";
+                }else if(space.length()==i){
+                    int j=i;
+                    while(j<=n){
+                        space+=symbol;
+                        j++;
+                    }
+                    System.out.println(space.substring(1));
+                    space="";
+                    break;
+                }
+            }
+        }
+    }
+}
+
